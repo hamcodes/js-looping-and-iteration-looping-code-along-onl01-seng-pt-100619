f7d0f2bd406f630b34588920af96522c0cd17850
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-04-22 15:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Code your solutions in this file
 const cards = ["Ada", "Brendan", "Ali",];
 
-function writeCards
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`);
+  }
+ 
+  return cards;
+}
+ 
+writeCards(cards);
